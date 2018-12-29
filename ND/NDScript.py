e28d07fce6e7a9a73c87e7ee82f53f0d69e7f127
openhearted99 <theodoreopena@gmail.com>
2018-12-29 13:04:32 -0500

    Changing project language to C++

--- ND/NDScript.py
+++ ND/NDScript.py
@@ -10,7 +10,10 @@ from random import randint
 
 
 # Constants
-len_hypercube = 1
+len_hypercube = 100
+birth_num = 3
+overcrowding_num = 4
+starvation_num = 2
 
 
 # Classes:
@@ -27,7 +30,7 @@ class Space(object):
                 sys.exit("Error! The dimensions of the space must be integers")
         elif len(dimensions) == 1:
             _size_cube = []
-            for i in range(dimensions[0]):
+            for i in np.arange(dimensions[0]):
                 _size_cube.append(len_hypercube)
             size_cube = tuple(_size_cube)
             self.space = np.zeros(size_cube, dtype=int)
@@ -41,10 +44,10 @@ class Space(object):
     # Seeds the gamespace by randomlly filling in 1% of the total cells.
     def seed_grid(self):
         num_entries2seed = randint(0, round(self.space.size*.01))
-        for i in range(num_entries2seed):
+        for i in np.arange(num_entries2seed):
             while True:
                 random_cell_coordinates = []
-                for j in range(self.space.ndim):
+                for j in np.arange(self.space.ndim):
                     random_cell_coordinates.append(randint(0,
                                                            self.space.shape[j]
                                                            - 1))
@@ -54,6 +57,16 @@ class Space(object):
                         self.space[tuple(random_cell_coordinates)] = 1
                         break
 
+    def find_neighbors(self, *cell_coordinates):
+        num_dim = self.space.ndim
+
+    def tick(self, iterations):
+        num_dim = self.space.num_dim
+        space_shape = self.space.shape
+        for iteration in np.arange(iterations):
+            _temp_space = np.zeros(space_shape, dtype=int)
+            for dimension in np.arange(num_dim):
+                for 
 
 if __name__ == '__main__':
     k = Space(3)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GeneralizingCGOL
+# Generalizing Conway's Game of Life
 A passion project from some undergaduates with too much time on their hands.
